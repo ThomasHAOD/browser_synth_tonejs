99c2257ae5064eaf716652d9e8150d90e8330746
Tam O'Donnell <thomashodonnell@gmail.com>
2019-05-19 16:21:30 +0100

    tone playing on click header

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ const Tone = require('Tone');
 document.addEventListener('DOMContentLoaded', () => {
     console.log('ready to rock');
 
-    var synth = new Tone.Synth().toMaster();
+    const synth = new Tone.Synth().toMaster();
     synth.triggerAttackRelease("C4", "8n");
-    document.querySelector('button').addEventListener('click', () => Tone.start())
+    const header = document.querySelector('header')
+    header.addEventListener('click', () => {Tone.start()})
 
 
 });
